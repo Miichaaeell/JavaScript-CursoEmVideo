e537379124e30c718119f7538efd81f1ece39b6e
Michael Andrew <96355111+Miichaaeell@users.noreply.github.com>
2024-09-01 14:22:02 -0300

    Cruso de JavaScript
    Auals do curso em video

---
+++ Condiçoes/ex008.js
@@ -0,0 +1,5 @@
+var vel = 12
+if (vel > 60) {
+    console.log(`Você ultrapassou a velocidade`)
+}
+console.log(`Dirija com cuidado`)
---
+++ Condiçoes/ex011.js
@@ -0,0 +1,10 @@
+idade = 23
+if (idade < 22){
+    console.log('menor de 22')
+}
+else if (idade == 22){
+    console.log('igual a 22')
+}
+else {
+    console.log('maior que 22')
+}
